026bfa05dbbf24b114e22cb8b8e0be743251b609
kvzsolt <32572393+kvzsolt@users.noreply.github.com>
2026-03-28 20:01:25 +0100

    properties added

---
+++ src/main/java/hu/agileexpert/smartos/config/SmartOsCorsProperties.java
@@ -0,0 +1,21 @@
+package hu.agileexpert.smartos.config;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "smartos.cors")
+@Getter
+@Setter
+public class SmartOsCorsProperties {
+
+    private List<String> allowedOrigins = List.of(
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://localhost:5173",
+            "http://127.0.0.1:5173"
+    );
+}
